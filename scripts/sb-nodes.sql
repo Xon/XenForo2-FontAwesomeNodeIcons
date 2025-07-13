9a288222a6d6ef2dcd8846e84498ec3e6b198cb1
Xon <635541+Xon@users.noreply.github.com>
2025-07-13 12:05:22 +0800

    scripts to populate SV/SB settings

---
+++ scripts/sb-nodes.sql
@@ -0,0 +1,103 @@
+-- 25th Anniversary Forum
+update xf_node set fa_node_icon = 'fa-birthday-cake' where node_id = 391;
+update xf_node set fa_node_icon = 'fa-birthday-cake' where node_id = 394;
+-- Active Global Conflicts
+update xf_node set fa_node_icon = 'fa-globe-africa' where node_id = 333;
+update xf_node set fa_node_icon = 'fa-globe-africa' where node_id = 397;
+-- Advisory Applications and Nominations
+update xf_node set fa_node_icon = 'fa-hand-holding-box' where node_id = 205;
+-- Anime & Manga
+update xf_node set fa_node_icon = 'fa-grin-beam-sweat' where node_id = 29;
+-- Appeals (Private)
+update xf_node set fa_node_icon = 'fa-balance-scale' where node_id = 70;
+-- Appeals Archive
+update xf_node set fa_node_icon = 'fa-folders' where node_id = 92;
+-- Art & Design
+update xf_node set fa_node_icon = 'fa-palette' where node_id = 5;
+-- Bargains, Barter & Benefactions
+update xf_node set fa_node_icon = 'fa-sack-dollar' where node_id = 58;
+-- Content Review (Private)
+update xf_node set fa_node_icon = 'fa-check-circle' where node_id = 98;
+-- Content Review Archive
+update xf_node set fa_node_icon = 'fa-folders' where node_id = 106;
+-- Contest Archives
+update xf_node set fa_node_icon = 'fa-folders' where node_id = 121;
+-- Contest Entries
+update xf_node set fa_node_icon = 'fa-mail-bulk' where node_id = 243;
+-- Contest Submissions
+update xf_node set fa_node_icon = 'fa-mail-bulk' where node_id = 133;
+update xf_node set fa_node_icon = 'fa-mail-bulk' where node_id = 399;
+-- Contests
+update xf_node set fa_node_icon = 'fa-vote-yea' where node_id = 78;
+-- Creative Content Review
+update xf_node set fa_node_icon = 'fa-inbox-in' where node_id = 272;
+-- Creative Writing
+update xf_node set fa_node_icon = 'fa-pen-nib' where node_id = 18;
+-- Creative Writing Archives
+update xf_node set fa_node_icon = 'fa-folders' where node_id = 40;
+-- From the Ashes
+update xf_node set fa_node_icon = 'fa-fire' where node_id = 51;
+-- Games & Gaming
+update xf_node set fa_node_icon = 'fa-gamepad-alt' where node_id = 21;
+-- General Content Review
+update xf_node set fa_node_icon = 'fa-inbox-in' where node_id = 263;
+-- General Fiction & Media
+update xf_node set fa_node_icon = 'fa-books' where node_id = 1;
+-- Grand Strategy
+update xf_node set fa_node_icon = 'fa-globe-stand' where node_id = 10;
+-- Image Threads
+update xf_node set fa_node_icon = 'fa-images' where node_id = 178;
+update xf_node set fa_node_icon = 'fa-images' where node_id = 352;
+-- Media Madness
+update xf_node set fa_node_icon = 'fa-film' where node_id = 25;
+-- News & Society Focused Discussion
+update xf_node set fa_node_icon = 'fa-newspaper' where node_id = 7;
+-- NSFD Content Review
+update xf_node set fa_node_icon = 'fa-inbox-in' where node_id = 269;
+-- Original Fiction
+update xf_node set fa_node_icon = 'fa-typewriter' where node_id = 48;
+-- Quests
+update xf_node set fa_node_icon = 'fa-poll-people' where node_id = 240;
+-- Roleplaying
+update xf_node set fa_node_icon = 'fa-theater-masks' where node_id = 74;
+-- Roleplaying IC
+update xf_node set fa_node_icon = 'fa-user-cowboy' where node_id = 60;
+-- Site News
+update xf_node set fa_node_icon = 'fa-megaphone' where node_id = 53;
+-- Site Suggestions
+update xf_node set fa_node_icon = 'fa-box-ballot' where node_id = 103;
+-- Staff Communication Archive
+update xf_node set fa_node_icon = 'fa-folders' where node_id = 100;
+-- Staff Communications (Private)
+update xf_node set fa_node_icon = 'fa-comment-alt-smile' where node_id = 75;
+-- Star Wars: Rebuilding of Empires
+update xf_node set fa_node_icon = 'fa-space-station-moon-alt' where node_id = 52;
+-- Story Ideas & Recommendations
+update xf_node set fa_node_icon = 'fa-lightbulb-on' where node_id = 63;
+update xf_node set fa_node_icon = 'fa-lightbulb-on' where node_id = 319;
+-- Story-Only
+update xf_node set fa_node_icon = 'fa-book' where node_id = 252;
+-- Strategy Archives
+update xf_node set fa_node_icon = 'fa-folders' where node_id = 15;
+-- Strategy IC
+update xf_node set fa_node_icon = 'fa-users-crown' where node_id = 255;
+-- Tech Talk
+update xf_node set fa_node_icon = 'fa-user-robot' where node_id = 16;
+-- Technical Analysis
+update xf_node set fa_node_icon = 'fa-chart-network' where node_id = 30;
+-- Technical Support
+update xf_node set fa_node_icon = 'fa-wrench' where node_id = 71;
+-- The War Room
+update xf_node set fa_node_icon = 'fa-fighter-jet' where node_id = 23;
+-- Unlisted Original Fiction
+update xf_node set fa_node_icon = 'fa-user-secret' where node_id = 169;
+-- Vs. Debates
+update xf_node set fa_node_icon = 'fa-graduation-cap' where node_id = 4;
+-- Vs. Debates Content Review
+update xf_node set fa_node_icon = 'fa-inbox-in' where node_id = 266;
+-- Water Cooler
+update xf_node set fa_node_icon = 'fa-comments' where node_id = 187;
+-- Whitehall - The Politics Forum!
+update xf_node set fa_node_icon = 'fa-dungeon' where node_id = 64;
+-- Worm
+update xf_node set fa_node_icon = 'fa-spider' where node_id = 115;
---
+++ scripts/sv-nodes.sql
@@ -0,0 +1,164 @@
+--  Forum News Category
+
+--  Forum News, Pending Announcements
+update xf_node set fa_node_icon = 'fa-crown' where node_id = 113;
+update xf_node set fa_node_icon = 'fa-crown' where node_id = 84;
+--  Announcement Archive
+update xf_node set fa_node_icon = 'fa-archive' where node_id = 71;
+--  Site Tutorials
+update xf_node set fa_node_icon = 'fa-info' where node_id = 132;
+
+--  Creative Forums Category
+
+--  Collaboration Station
+update xf_node set fa_node_icon = 'fa-compress-arrows-alt' where node_id = 78;
+--  Creative Discussion & Worldbuilding (including link from Quests)
+update xf_node set fa_node_icon = 'fa-construction' where node_id = 56;
+update xf_node set fa_node_icon = 'fa-construction' where node_id = 66;
+--  User Fiction
+update xf_node set fa_node_icon = 'fa-book-open' where node_id = 2;
+--  Original Fiction
+update xf_node set fa_node_icon = 'fa-pen-nib' where node_id = 157;
+--  Unlisted Fiction
+update xf_node set fa_node_icon = 'fa-book' where node_id = 160;
+--  Unlisted Fiction
+update xf_node set fa_node_icon = 'fa-book' where node_id = 15;
+--  Non-threadmarked content
+update xf_node set fa_node_icon = 'fa-book-alt' where node_id = 98;
+--  Weird History (+2 links)
+update xf_node set fa_node_icon = 'fa-bat' where node_id = 95;
+update xf_node set fa_node_icon = 'fa-bat' where node_id = 96;
+update xf_node set fa_node_icon = 'fa-bat' where node_id = 97;
+--  Fanworks Discussion is a link to Fanfiction discussion, see below
+--  Worm
+update xf_node set fa_node_icon = 'fa-spider' where node_id = 94;
+--  User Fiction Archive
+update xf_node set fa_node_icon = 'fa-books' where node_id = 31;
+--  Quests
+update xf_node set fa_node_icon = 'fa-vote-nay' where node_id = 29;
+--  Quests Archive
+update xf_node set fa_node_icon = 'fa-comment-alt-times' where node_id = 17;
+--  Art and Music
+update xf_node set fa_node_icon = 'fa-palette' where node_id = 9;
+
+--  Roleplaying Category
+
+--  Tabletop & Play-by-Post Roleplaying
+update xf_node set fa_node_icon = 'fa-chess' where node_id = 82;
+--  Game Recruitment/OOC Threads
+update xf_node set fa_node_icon = 'fa-universal-access' where node_id = 4;
+--  Game System Discussions
+update xf_node set fa_node_icon = 'fa-cogs' where node_id = 118;
+--  In Progress Games
+update xf_node set fa_node_icon = 'fa-dice-d20' where node_id = 16;
+--  Play-by-Post Games
+update xf_node set fa_node_icon = 'fa-dice-d12' where node_id = 121;
+--  Playtesting and Experimental Games
+update xf_node set fa_node_icon = 'fa-dice-d4' where node_id = 127;
+--  Mafia and Forum Games
+update xf_node set fa_node_icon = 'fa-knife-kitchen' where node_id = 124;
+--  Grand Strategy Roleplaying
+update xf_node set fa_node_icon = 'fa-globe-stand' where node_id = 49;
+--  Grand Strategy IC
+update xf_node set fa_node_icon = 'fa-hood-cloak' where node_id = 57;
+--  Grand Strategy Archive
+update xf_node set fa_node_icon = 'fa-atlas' where node_id = 58;
+
+--  General Discussion Category
+
+--  Fiction Discussion
+update xf_node set fa_node_icon = 'fa-book-heart' where node_id = 10;
+--  Fanfiction Discussion (+1 link)
+update xf_node set fa_node_icon = 'fa-magic' where node_id = 27;
+update xf_node set fa_node_icon = 'fa-magic' where node_id = 30;
+--  TV, Movies, and Anime Discussion
+update xf_node set fa_node_icon = 'fa-tv' where node_id = 33;
+--  Contest Archive, 2021, 2022...
+update xf_node set fa_node_icon = 'fa-medal' where node_id = 145;
+update xf_node set fa_node_icon = 'fa-medal' where node_id = 148;
+update xf_node set fa_node_icon = 'fa-medal' where node_id = 149;
+update xf_node set fa_node_icon = 'fa-medal' where node_id = 150;
+--  Versus Debates
+update xf_node set fa_node_icon = 'fa-swords-laser' where node_id = 7;
+--  Research & History
+update xf_node set fa_node_icon = 'fa-map-signs' where node_id = 6;
+--  Weird History links in Creative Forums category
+--  Warfare
+update xf_node set fa_node_icon = 'fa-swords' where node_id = 90;
+--  Active Conflicts (+1 link)
+update xf_node set fa_node_icon = 'fa-fire-alt' where node_id = 139;
+update xf_node set fa_node_icon = 'fa-fire-alt' where node_id = 142;
+--  Alternate History
+update xf_node set fa_node_icon = 'fa-project-diagram' where node_id = 91;
+--  Science and Technology
+update xf_node set fa_node_icon = 'fa-flask' where node_id = 89;
+--  Gaming
+update xf_node set fa_node_icon = 'fa-game-board' where node_id = 70;
+--  Video Gaming
+update xf_node set fa_node_icon = 'fa-gamepad' where node_id = 8;
+--  Chat Lounge
+update xf_node set fa_node_icon = 'fa-couch' where node_id = 38;
+--  Image Threads
+update xf_node set fa_node_icon = 'fa-images' where node_id = 164;
+--  Hobbies
+update xf_node set fa_node_icon = 'fa-paper-plane' where node_id = 65;
+--  News & Politics
+update xf_node set fa_node_icon = 'fa-newspaper' where node_id = 169;
+update xf_node set fa_node_icon = 'fa-newspaper' where node_id = 5;
+--  Politics
+update xf_node set fa_node_icon = 'fa-handshake' where node_id = 166;
+--  Active Conflicts link under Warfare above
+--  US Affairs
+update xf_node set fa_node_icon = 'fa-flag-usa' where node_id = 101;
+--  Coronavirus COVID-19 Pandemic
+update xf_node set fa_node_icon = 'fa-head-side-mask' where node_id = 104;
+--  I'm Not Owned, You're Owned
+update xf_node set fa_node_icon = 'fa-corn' where node_id = 156;
+
+--  Forum Governance Category
+
+--  Staff Communications
+update xf_node set fa_node_icon = 'fa-comment-lines' where node_id = 12;
+--  Ask a Private Question
+update xf_node set fa_node_icon = 'fa-user-shield' where node_id = 46;
+--  Resolved Questions and Reviews
+update xf_node set fa_node_icon = 'fa-stamp' where node_id = 48;
+--  Internal advertisements
+update xf_node set fa_node_icon = 'fa-ad' where node_id = 68;
+update xf_node set fa_node_icon = 'fa-ad' where node_id = 69;
+--  Forum Technical Support
+update xf_node set fa_node_icon = 'fa-hand-heart' where node_id = 26;
+--  Suggestion Box, Archived Suggestions
+update xf_node set fa_node_icon = 'fa-box-ballot' where node_id = 80;
+update xf_node set fa_node_icon = 'fa-box-ballot' where node_id = 81;
+--  Administrative Tribunal
+update xf_node set fa_node_icon = 'fa-gavel' where node_id = 18;
+--  Make an Appeal
+update xf_node set fa_node_icon = 'fa-scroll' where node_id = 23;
+--  Resolved Appeals
+update xf_node set fa_node_icon = 'fa-scroll-old' where node_id = 40;
+--  Get an Advocate
+update xf_node set fa_node_icon = 'fa-angel' where node_id = 130;
+--  Tribunal Deliberations
+update xf_node set fa_node_icon = 'fa-landmark' where node_id = 51;
+--  Tribunal non-public Archive
+update xf_node set fa_node_icon = 'fa-dumpster-fire' where node_id = 67;
+--  In-Camera
+update xf_node set fa_node_icon = 'fa-balance-scale' where node_id = 35;
+--  Council Commentary
+update xf_node set fa_node_icon = 'fa-webcam' where node_id = 161;
+--  Teams
+update xf_node set fa_node_icon = 'fa-users' where node_id = 109;
+--  Staff Discussion
+update xf_node set fa_node_icon = 'fa-lock-alt' where node_id = 14;
+--  Survey Responses
+update xf_node set fa_node_icon = 'fa-poll-people' where node_id = 77;
+--  Recycle Bin
+update xf_node set fa_node_icon = 'fa-trash' where node_id = 37;
+--  User Reviews
+update xf_node set fa_node_icon = 'fa-user-secret' where node_id = 83;
+--  Conversation Archive
+update xf_node set fa_node_icon = 'fa-book-user' where node_id = 79;
+
+--  Event Stuff.
+update xf_node set fa_node_icon = 'fa-comet' where node_id = 138;
